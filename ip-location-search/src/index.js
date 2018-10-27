1cc4fac911d62a6177ff57d8b2cee23c28042a79
orbit-stabilizer <deowra.kamal@gmail.com>
2018-10-27 10:33:37 -0700

    trying to get mapbox to work

--- ip-location-search/src/index.js
+++ ip-location-search/src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 const API_KEY = 'b6b77e811c529d0c1d594b9954eb3b5a';
+const MAPBOX_API_KEY = 'sk.eyJ1Ijoia2Rlb3dyYSIsImEiOiJjam5xenBmM3EwMzFwM3Bxend6bWFuamc5In0.GGMg0SpiXVELreGJ6PJCxw';
 
 class App extends Component {
 	constructor(props) {
@@ -9,13 +10,16 @@ class App extends Component {
 
 		this.state = {
 			term: '',
-			city: '',
-			country: '',
+			longitude: '',
+			latitude: '',
+			map: '',
 			error: null
 		};
 	}
 
-	onButtonClick() {
+	onFormSubmit(event) {
+		event.preventDefault();
+
 		fetch(`http://api.ipstack.com/${this.state.term}?access_key=${API_KEY}`)
 			.then((res) => {
 				if (res.ok) {
@@ -27,22 +31,38 @@ class App extends Component {
 			.then((response) => response.json())
 			.then((data) => {
 				console.log(data);
-				console.log(data.city);
-				console.log(data.country_name);
-				console.log(this);
-				return this.setState({ city: data.city, country: data.country_name });
+				return this.setState({ longitude: data.longitude, latitude: data.latitude }, () =>
+					fetch(
+						`http://api.mapbox.com/v4/mapbox.mapbox-streets-v7/zoomwheel,zoompan.html?access_token=${MAPBOX_API_KEY}#zoom/${this
+							.state.latitude}/${this.state.longitude}`
+					)
+						.then((res) => res.text())
+						.then((map) => this.setState({ map }))
+				);
 			})
 			.catch((error) => this.setState({ error }));
 	}
 
 	render() {
 		return (
-			<div>
-				<input value={this.state.term} onChange={(event) => this.setState({ term: event.target.value })} />
-				<button onClick={() => this.onButtonClick()}>Submit</button>
-				<p>City: {this.state.city}</p>
-				<p>Country: {this.state.country}</p>
-				<p>error: {this.state.error}</p>
+			<div className="container">
+				<form onSubmit={(event) => this.onFormSubmit(event)}>
+					<div className="form-group">
+						<input
+							className="form-control"
+							onChange={(event) => this.setState({ term: event.target.value })}
+							placeholder="Enter IP Address"
+							value={this.state.term}
+						/>
+						<button className="btn btn-primary btn-block mt-3" type="submit">
+							Submit
+						</button>
+					</div>
+				</form>
+				<iframe
+					src={`http://api.mapbox.com/v4/mapbox.mapbox-streets-v7/zoomwheel,zoompan.html?access_token=${MAPBOX_API_KEY}#zoom/${this
+						.state.latitude}/${this.state.longitude}`}
+				/>
 			</div>
 		);
 	}
